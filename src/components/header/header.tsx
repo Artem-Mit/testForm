759a8b2449efd76de315683a1e6722ab7056d9a8
Artem <artem.v.mitin@yandex.ru>
2024-04-17 13:04:13 +0500

    add timerstate to localstorage

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import Countdown from "react-countdown";
 import { timeConverter } from "@/helpers/timeConverter";
 import { Test } from "@/api";
-import styles from './header.module.css';
+import styles from "./header.module.css";
 
 type Props = {
   test: Test;
@@ -10,15 +11,37 @@ type Props = {
 
 export const Header = ({ test, onComplete }: Props) => {
   const time = timeConverter(test.time);
+  const [timeOnTimer, setTimeOnTimer] = useState(time);
+
+  const onTick = () => {
+    if (timeOnTimer > 0) {
+      setTimeOnTimer((prev) => prev - 100);
+      localStorage.setItem("time", JSON.stringify(timeOnTimer));
+    }
+  };
+
+  const setActualTime = () => {
+    const actualTime = localStorage.getItem("time");
+    if (actualTime) setTimeOnTimer(JSON.parse(actualTime));
+  };
+
+  const onEnd = () => {
+    setTimeOnTimer(0);
+    localStorage.setItem("time", JSON.stringify(0));
+    onComplete();
+  };
 
   return (
     <div className={styles.titleContainer}>
       <h1 className={styles.header}>{test.name}</h1>
       <Countdown
-        date={Date.now() + time}
+        date={Date.now() + timeOnTimer}
         daysInHours
         className={styles.counter}
-        onComplete={onComplete}
+        onComplete={onEnd}
+        onTick={onTick}
+        onMount={setActualTime}
+        intervalDelay={100}
       />
     </div>
   );
